ebfd4d3be81505f5eb1738e5bccdd092661cd61f
Touchscreen PC <sageroboticsbyhalia@gmail.com>
2025-10-20 21:40:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scrolling effect for internal links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
